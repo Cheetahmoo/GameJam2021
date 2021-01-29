535d14a197ea15ef33e04978927ee5cfbe6a79d1
Josiah <Cheetahmoo@gmail.com>
2021-01-29 16:24:29 -0500

    Add New Classes

---
+++ lostAndFound/Assets/Engine/Controllers/RoomController.cs
@@ -0,0 +1,16 @@
+﻿using Engine.DataTypes;
+using UnityEngine;
+
+namespace Engine.Controllers
+{
+    public class RoomController : MonoBehaviour
+    {
+        public Room CurrentRoom { get; private set; }
+        
+        //Controllers
+        public EnemyController EC;
+        
+        
+        
+    }
+}
---
+++ lostAndFound/Assets/Engine/GameManger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Engine
+{
+    public class GameManger : MonoBehaviour
+    {
+        public int NumRooms { get; private set; } = 2;
+    }
+}
