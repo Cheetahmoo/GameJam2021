11bd258c063df17e8788562731329f2727bacf85
Tintinabulum <60529494+Tintinabulum@users.noreply.github.com>
2021-01-30 18:18:02 -0500

    Pause Menu

---
+++ lostAndFound/Assets/Scripts/PauseController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseController : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject[] menu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        foreach (GameObject obj in menu) obj.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        menu[0].SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene("Menu");//Menu
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
