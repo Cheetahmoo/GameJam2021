2440a563d6d720d6817cbf9e69f53a3115a066e9
Bob Bobbington <popyd10@gmail.com>
2021-01-29 19:21:44 -0500

    Scene setup

--- lostAndFound/Assets/Scripts/Health.cs
+++ lostAndFound/Assets/Scripts/Health.cs
@@ -11,13 +11,13 @@ public class Health : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(hasInitialized && health <= 0)
+        if (hasInitialized && health <= 0)
         {
             Kill();
         }
